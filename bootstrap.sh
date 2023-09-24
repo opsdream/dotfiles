dd8fc0bf8422b4a4136824ec3b965b0697254b92
Opsdream <96179360+opsdream@users.noreply.github.com>
2023-09-24 12:27:48 +0100

    update bootstrapper

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y  net-tools curl software-properties-common apt-transport-https ca-certificates gnupg2 jq unzip zip
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install packer -y
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo apt install bash-completion -y
+sudo echo "source /etc/profile.d/bash_completion.sh" >> ~/.bashrc
+
+terraform -install-autocomplete
+packer -autocomplete-install
