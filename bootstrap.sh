691535775e083719a6041f360f6c55b728681fb0
Opsdream <96179360+opsdream@users.noreply.github.com>
2023-09-24 12:32:51 +0100

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 sudo apt update
-sudo apt install -y  net-tools curl software-properties-common apt-transport-https ca-certificates gnupg2 jq unzip zip
-
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform -y
-
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install packer -y
+sudo apt install -y  net-tools curl software-properties-common apt-transport-https ca-certificates gnupg2 jq unzip zip terraform packer
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -19,3 +14,5 @@ sudo echo "source /etc/profile.d/bash_completion.sh" >> ~/.bashrc
 
 terraform -install-autocomplete
 packer -autocomplete-install
+
+sudo rm -r 'sudo apt update && sudo apt install packer' 
